3fdfba03b4618f5ad0fb32bc20bce698a30d4585
orfevr <dtgraf@yandex.ru>
2014-12-19 18:15:11 +0200

    InitCommit

---
+++ CompiledFeeds/CompiledFeeds.Tests/CoreTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace CompiledFeeds.Tests
+{
+    [TestFixture]
+    public class CoreTests
+    {
+        [TestFixtureSetUp]
+        public void StartUp()
+        {
+        }
+
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+        }
+    }
+}
